0d80306cbefd5e67ec3c0cf7fd75bb7785c059cb
ECE36 <ECE36@users.noreply.github.com>
2025-04-21 14:01:19 -0400

    Update main.m

--- main.m
+++ main.m
@@ -1,7 +1,9 @@
 %% main.m
-% Creates all figures for Radar2025 manuscript
-close all
+% Creates three figures for Radar2025 manuscript and one for oral
+% presentation
+close all; clearvars
 
 Figure1Script
 Figure2Script
 Figure3Script
+CompareSolvers
